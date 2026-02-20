495361ba9ec2f3584373603f9d124ca5b36f0592
RaghadNasiaf <naseefraghad@gmail.com>
2026-02-20 21:52:50 +0000

    Fix measure_runtime: time.time with explicit gather

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,19 +2,19 @@
 """Module that contains measure_runtime coroutine."""
 
 import asyncio
-from time import perf_counter
+import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """Execute async_comprehension four times in parallel and measure runtime."""
-    start = perf_counter()
+    start = time.time()
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
-        async_comprehension(),
+        async_comprehension()
     )
-    end = perf_counter()
+    end = time.time()
     return end - start
