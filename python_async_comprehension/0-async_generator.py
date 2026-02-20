ed7ebf87e066cec2823751ddfa85251ff169c73b
RaghadNasiaf <naseefraghad@gmail.com>
2026-02-20 14:21:34 +0000

    Fix Task 0 async generator style

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module that defines an asynchronous generator."""
+"""Async generator that yields random floats with an asynchronous delay."""
 
 import asyncio
 import random
@@ -7,7 +7,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Yield 10 random numbers between 0 and 10 asynchronously."""
+    """Yield 10 random floats between 0 and 10, waiting 1 second each time."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
