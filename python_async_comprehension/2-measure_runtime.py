1c4ed4b06e92ea4cc331ade3eb06c978b06efc88
RaghadNasiaf <naseefraghad@gmail.com>
2026-02-20 22:10:24 +0000

    Fix import style for time

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,21 +2,18 @@
 """Module that contains measure_runtime coroutine."""
 
 import asyncio
-import time
+from time import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """Execute async_comprehension four times in parallel and measure runtime."""
-    start = time.time()
-
-    task1 = asyncio.create_task(async_comprehension())
-    task2 = asyncio.create_task(async_comprehension())
-    task3 = asyncio.create_task(async_comprehension())
-    task4 = asyncio.create_task(async_comprehension())
-
-    await asyncio.gather(task1, task2, task3, task4)
-
-    end = time.time()
-    return end - start
+    start = time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+    )
+    return time() - start
