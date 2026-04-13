4849545c1e60493ba87670c767cf60050860628f
RaghadNasiaf <raghad@example.com>
2026-04-13 08:34:42 +0000

    Task 7: Move file to correct directory and setup server

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+const databaseFile = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(databaseFile)
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students\n${err.message}`);
+    });
+});
+
+app.listen(port, () => {
+});
+
+module.exports = app;
