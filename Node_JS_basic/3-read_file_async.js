e3f5dae1fa0c2d754749fa18b50a8511ae7e7260
RaghadNasiaf <raghad@example.com>
2026-04-13 08:15:26 +0000

    Add task 3: Asynchronous file reading with Promises

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.trim().split('\n').filter((line) => line.length > 0);
+      const studentLines = lines.slice(1);
+      console.log(`Number of students: ${studentLines.length}`);
+
+      const fields = {};
+      studentLines.forEach((line) => {
+        const student = line.split(',');
+        if (student.length === 4) {
+          const firstName = student[0];
+          const field = student[3];
+
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstName);
+        }
+      });
+
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          const list = fields[field].join(', ');
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${list}`);
+        }
+      }
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
